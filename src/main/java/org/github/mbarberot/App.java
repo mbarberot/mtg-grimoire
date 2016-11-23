27d2b6b7ccc5f632a1c9300ce8faa4b5473c06b9
mbarberot <mbarberot@users.noreply.github.com>
2016-11-23 13:18:29 +0100

    [/index] Basic index page - using jade templates - using custom semantic-ui + a custom css file

--- src/main/java/org/github/mbarberot/App.java
+++ src/main/java/org/github/mbarberot/App.java
@@ -1,9 +1,17 @@
 package org.github.mbarberot;
 
+import spark.ModelAndView;
+import spark.template.jade.JadeTemplateEngine;
+
+import static java.util.Collections.emptyMap;
 import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.staticFiles;
 
 public interface App {
     static void main(String[] args) {
-        get("/", (request, response) -> "Hello World");
+        staticFiles.location("/public");
+                
+        get("/", (request, response) -> new ModelAndView(emptyMap(), "pages/index"), new JadeTemplateEngine());
     }
 }

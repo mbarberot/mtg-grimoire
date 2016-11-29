c3c500e84ed1951b7ea3f42ad3e99703ea8beec8
mbarberot <mbarberot@users.noreply.github.com>
2016-11-29 15:26:11 +0100

    [war] Ship app as a .war

--- src/main/java/org/github/mbarberot/App.java
+++ src/main/java/org/github/mbarberot/App.java
@@ -1,20 +1,28 @@
 package org.github.mbarberot;
 
 import spark.ModelAndView;
+import spark.servlet.SparkApplication;
 import spark.template.jade.JadeTemplateEngine;
 
 import static java.util.Collections.emptyMap;
 import static spark.Spark.*;
 
-public interface App {
-    static void main(String[] args) {
+public class App implements SparkApplication {
+    
+    public static void main(String[] args) {
+        App app = new App();
+        app.init();
+    }
+
+    @Override
+    public void init() {
         staticFiles.location("/public");
         port(8080);
 
         JadeTemplateEngine jadeTemplateEngine = new JadeTemplateEngine();
 
-        get("/search", (request, response)      -> new ModelAndView(emptyMap(), "pages/search"), jadeTemplateEngine);
-        get("/cards/:id", (request, response)   -> new ModelAndView(emptyMap(), "pages/cards/card"), jadeTemplateEngine);
-        get("/", (request, response)            -> new ModelAndView(emptyMap(), "pages/index"), jadeTemplateEngine);
+        get("/search", (request, response) -> new ModelAndView(emptyMap(), "pages/search"), jadeTemplateEngine);
+        get("/cards/:id", (request, response) -> new ModelAndView(emptyMap(), "pages/cards/card"), jadeTemplateEngine);
+        get("/", (request, response) -> new ModelAndView(emptyMap(), "pages/index"), jadeTemplateEngine);
     }
 }

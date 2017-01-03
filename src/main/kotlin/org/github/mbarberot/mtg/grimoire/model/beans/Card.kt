980a493c7b4a00801956aa3236b4167b203ee2b3
mbarberot <mbarberot@users.noreply.github.com>
2017-01-03 17:18:23 +0100

    [search] Implement dynamic search

---
+++ src/main/kotlin/org/github/mbarberot/mtg/grimoire/model/beans/Card.kt
@@ -0,0 +1,7 @@
+package org.github.mbarberot.mtg.grimoire.model.beans
+
+class Card(
+        val name: String,
+        val imageUrl: String = "https://hydra-media.cursecdn.com/mtgsalvation.gamepedia.com/thumb/f/f8/Magic_card_back.jpg/172px-Magic_card_back.jpg"
+){
+}
--- src/main/kotlin/org/github/mbarberot/mtg/grimoire/spark/App.kt
+++ src/main/kotlin/org/github/mbarberot/mtg/grimoire/spark/App.kt
@@ -3,6 +3,7 @@ package org.github.mbarberot.mtg.grimoire.spark
 import com.mongodb.MongoClient
 import com.mongodb.MongoClientURI
 import org.github.mbarberot.mtg.grimoire.misc.config.Configuration
+import org.github.mbarberot.mtg.grimoire.model.beans.Card
 import org.jongo.JongoNative
 import spark.ModelAndView
 import spark.Spark.*
@@ -15,7 +16,7 @@ object App {
         val config = Configuration()
 
         configure(config)
-        declareRoutes(config)
+        declareRoutes()
     }
 
     private fun configure(config: Configuration) {
@@ -23,16 +24,37 @@ object App {
         staticFiles.location("/public")
     }
 
-    private fun declareRoutes(config: Configuration) {
+    private fun declareRoutes() {
         val templateEngine = JadeTemplateEngine()
-        //val dbClient = initDatabaseClient(config)
+        val cards = listOf(
+                Card("Ajani Goldmane"),
+                Card("Archangel Avacyn"),
+                Card("Arlin Kord"),
+                Card("Ashlok, NightMare Weaver"),
+                Card("Ashling the Pilgrim"),
+                Card("Ashling, the Extinguisher")
+        )
+        
+        post("/ic/search", { req, res ->
+            val query = req.queryParams("q")
+            ModelAndView(
+                    mapOf(Pair("cards", cards.filter { card -> card.name.toLowerCase().startsWith(query.toLowerCase()) })),
+                    "parts/search-results"
+            ) 
+        }, templateEngine)
         
         get("/", { request, response -> ModelAndView(Collections.emptyMap<Any, Any>(), "pages/index") }, templateEngine)
+        
     }
 
-    /*fun initDatabaseClient(config: Configuration): JongoNative {
-        val mongoClient = MongoClient(MongoClientURI(config.getDatabaseURL()))
-        val database = mongoClient.getDatabase(config.getDatabaseName())
-        return JongoNative(database)
-    }*/
 }
+
+/*
+val dbClient = initDatabaseClient(config)
+fun initDatabaseClient(config: Configuration): JongoNative {
+    val mongoClient = MongoClient(MongoClientURI(config.getDatabaseURL()))
+    val database = mongoClient.getDatabase(config.getDatabaseName())
+    return JongoNative(database)
+}
+*/
+

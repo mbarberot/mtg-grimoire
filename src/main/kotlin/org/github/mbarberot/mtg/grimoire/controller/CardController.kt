0d031bbd97fd010b905ca5a62992b6cf461a09cb
mbarberot <mbarberot@users.noreply.github.com>
2017-01-18 22:26:14 +0100

    Setup model for pagination

--- src/main/kotlin/org/github/mbarberot/mtg/grimoire/controller/CardController.kt
+++ src/main/kotlin/org/github/mbarberot/mtg/grimoire/controller/CardController.kt
@@ -5,6 +5,14 @@ import org.github.mbarberot.mtg.grimoire.view.View
 
 class CardController(val cardManager: CardManager) : AbstractController() {
     fun getCard(id: String, view: View): Any = render(view, "parts/card", "card", cardManager.getCardById(id))
-    fun getCards(query: String, view: View): Any = render(view, "parts/search-results", "cards", cardManager.searchCards(query))
+    fun getCards(query: String, view: View): Any =
+            render(
+                    view,
+                    "parts/search-results",
+                    mapOf(
+                            Pair("cards", cardManager.searchCards(query)),
+                            Pair("pages", cardManager.countCards(query))
+                    )
+            )
 }
 
--- src/main/kotlin/org/github/mbarberot/mtg/grimoire/model/managers/CardManager.kt
+++ src/main/kotlin/org/github/mbarberot/mtg/grimoire/model/managers/CardManager.kt
@@ -4,13 +4,24 @@ import org.github.mbarberot.mtg.grimoire.model.beans.Card
 import org.jongo.JongoNative
 
 class CardManager(val jongo: JongoNative) {
-    fun searchCards(query: String): Collection<Card> {
+    fun searchCards(query: String, page: Int = 1, size: Int = 10): Collection<Card> {
         return if (query.isNotEmpty()) {
+            val offset = (page - 1) * size
             getCollection()
                     .find(jongo.query("{name: {\$regex: #, \$options: 'i'}}", ".*$query.*"))
+                    .skip(offset)
+                    .limit(size)
                     .toList()
         } else {
             emptyList()
+        }
+    }
+
+    fun countCards(query: String): Long {
+        return if (query.isNotEmpty()) {
+            getCollection().count(jongo.query("{name: {\$regex: #, \$options: 'i'}}", ".*$query.*"))
+        } else {
+            0
         }
     }
 
@@ -24,4 +35,5 @@ class CardManager(val jongo: JongoNative) {
     fun removeAll() = getCollection().deleteMany(jongo.query("{}"))
 
     private fun getCollection() = jongo.getCollection("cards", Card::class.java)
+
 }

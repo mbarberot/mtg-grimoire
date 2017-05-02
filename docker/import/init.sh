72ffee6394e7c0cae8abfebc95d1f0a18bd1eaa2
mbarberot <mbarberot@users.noreply.github.com>
2017-05-02 17:25:03 +0200

    Wipe old docker files

--- docker/import/init.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker exec -it mtg_grimoire_database mongo admin --eval 'db.getSiblingDB("mtg_grimoire").createUser({user: "test", pwd: "test", roles: [{ role: "dbOwner", db: "mtg_grimoire" }]})'
